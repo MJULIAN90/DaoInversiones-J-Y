8aeb32f5f99b4f11a6db14cf03378d0daf909e1b
Julian Ruiz <michaeljulian50@gmail.com>
2026-04-01 22:29:22 -0500

    Feat:add, initial proyect & TimeLock

---
+++ src/governance/TimeLock.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+
+  // ==========================================================
+  //                      CONSTRUCTOR
+  // ==========================================================
+  
+  constructor(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors,
+    address admin
+  ) TimelockController(
+      minDelay, 
+      proposers,
+      executors,
+      admin
+    ){}
+}
